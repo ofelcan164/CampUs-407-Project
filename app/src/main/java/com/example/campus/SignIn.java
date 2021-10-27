4323686b95cf22d350021641d1bc9b73d5326590
Oscar Felcan <ofelcan164@gmail.com>
2021-10-27 12:57:35 -0500

    First basic Create Profile screen (need to fix Constraints of Signin). Added some styles (GenTxt, GenBtnTxt, GenEditTxt)

--- app/src/main/java/com/example/campus/SignIn.java
+++ app/src/main/java/com/example/campus/SignIn.java
@@ -2,13 +2,40 @@ package com.example.campus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignIn extends AppCompatActivity {
+
+    Button signin;
+    Button createProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signin);
+
+        // Configure on screen buttons
+        signin = (Button) findViewById(R.id.signin_btn);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        createProfile = (Button) findViewById(R.id.create_profile_btn);
+        createProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createProfileClicked();
+            }
+        });
+    }
+
+    public void createProfileClicked() {
+        Intent intent = new Intent(this, CreateProfile.class);
+        startActivity(intent);
     }
 }
