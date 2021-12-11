d522189555312c42e845da46f2616d9d42ce7a88
Collin Krause <cpkrause2@wisc.edu>
2021-12-11 16:42:35 -0600

    Added urgency rating threshold using notification channels. Go into the setting app to select which notifications you want

--- app/src/main/java/com/example/campus/App.java
+++ app/src/main/java/com/example/campus/App.java
@@ -10,27 +10,63 @@ import androidx.core.app.NotificationCompat;
 public class App extends Application {
 
     public static final String CHANNEL_1_ID = "channel1";
+    public static final String CHANNEL_2_ID = "channel2";
+    public static final String CHANNEL_3_ID = "channel3";
+    public static final String CHANNEL_4_ID = "channel4";
+    public static final String CHANNEL_5_ID = "channel5";
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        createNotificationChannel();
+        createNotificationChannels();
     }
 
-    private void createNotificationChannel() {
+    private void createNotificationChannels() {
 
         // Check Device Build Number
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel1 = new NotificationChannel(
                     CHANNEL_1_ID,
-                    "Channel 1",
+                    "Urgency Rating: !",
                     NotificationManager.IMPORTANCE_HIGH);
 
-            channel1.setDescription("This is Channel 1");
+            channel1.setDescription("This is Urgency Rating 1 Channel");
 
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel1);
+
+            NotificationChannel channel2 = new NotificationChannel(
+                    CHANNEL_2_ID,
+                    "Urgency Rating: !!",
+                    NotificationManager.IMPORTANCE_HIGH);
+
+            channel1.setDescription("This is Urgency Rating 2 Channel");
+            manager.createNotificationChannel(channel2);
+
+            NotificationChannel channel3 = new NotificationChannel(
+                    CHANNEL_3_ID,
+                    "Urgency Rating: !!!",
+                    NotificationManager.IMPORTANCE_HIGH);
+
+            channel1.setDescription("This is Urgency Rating 3 Channel");
+            manager.createNotificationChannel(channel3);
+
+            NotificationChannel channel4 = new NotificationChannel(
+                    CHANNEL_4_ID,
+                    "Urgency Rating: !!!!",
+                    NotificationManager.IMPORTANCE_HIGH);
+
+            channel1.setDescription("This is Urgency Rating 4 Channel");
+            manager.createNotificationChannel(channel4);
+
+            NotificationChannel channel5 = new NotificationChannel(
+                    CHANNEL_5_ID,
+                    "Urgency Rating: !!!!!",
+                    NotificationManager.IMPORTANCE_HIGH);
+
+            channel1.setDescription("This is Urgency Rating 5 Channel");
+            manager.createNotificationChannel(channel5);
         }
     }
 }
